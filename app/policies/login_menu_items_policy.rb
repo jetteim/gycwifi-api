369828f566d14cd0ce208ebf930c7affe4711743
Anton Smagin <>
2017-08-23 16:50:13 +0300

    pundit scope fix

--- app/policies/login_menu_items_policy.rb
+++ app/policies/login_menu_items_policy.rb
@@ -22,7 +22,7 @@ class LoginMenuItemPolicy < ApplicationPolicy
 
   class Scope < Scope
     def resolve
-      return scope if user.super_user?
+      return scope.all if user.super_user?
       scope.where(location_id: Pundit.policy_scope!(user, Location).pluck(:id))
     end
   end
