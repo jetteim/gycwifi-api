00f50afec9f2b9eb87672d7477c13a1ccca52a09
Maxim Lee <mlee@key-g.com>
2017-08-28 18:45:27 +0300

    profile page#save ->users_controller#update -> update profile instead of user

--- app/controllers/dashboard/users_controller.rb
+++ app/controllers/dashboard/users_controller.rb
@@ -59,18 +59,8 @@ module Dashboard
 
     def update
       authorize(@object_user)
-      if @object_user.update(profile_params)
-        render json: {
-          {
-            id: @object_user.id,
-            auth: true,
-            username: @object_user.username,
-            email: @object_user.email,
-            avatar: @object_user.avatar,
-            token: token(@object_user.id),
-            role: @object_user.role,
-            user_info: @object_user.front_model
-          }
+      if @object_user.update(user_params)
+        render json: @object_user.front_model
       else
         render json: { error: user.errors.full_messages }
       end
@@ -197,7 +187,7 @@ module Dashboard
     end
 
     def profile_params
-      params.require(:profile).permit(:password, :avatar, :type, :tour, :email, :user)
+      params.require(:user).permit(:password, :avatar, :type, :tour, :email, :user)
     end
   end
 end
