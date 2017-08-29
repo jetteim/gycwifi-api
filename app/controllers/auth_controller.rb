3582871421692027cf4dffd64b110e73d293553d
Maxim Lee <mlee@key-g.com>
2017-08-29 10:47:48 +0300

    twitter request_token method

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -86,7 +86,7 @@ class AuthController < ApplicationController #:nodoc:
   def oauth1_request_token(auth_data)
     case auth_data[:provider]
     when 'twitter'
-      Oauth::TwitterLibrary.get_request_token(Rails.url_for(only_path: false))
+      Oauth::TwitterLibrary.get_request_token(request.original_url)
     end
   end
 
