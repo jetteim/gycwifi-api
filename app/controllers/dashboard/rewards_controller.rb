d2d66dece2e4eaa43eb429133e36fb19758d0b38
Maxim Lee <mlee@key-g.com>
2017-08-29 20:52:08 +0300

    using oauth gem

---
+++ app/controllers/dashboard/rewards_controller.rb
@@ -0,0 +1,27 @@
+module Dashboard
+  # Rewards Controller
+  class RewardsController < ApplicationController
+    around_action :skip_bullet, only: :index # gem bullet выдает unused eager loading но это неправда(баг)
+    PAGESIZE = 20
+    def index
+      agent_rewards = policy_scope(AgentReward).includes(user: :promo_code)
+      render json: {
+        items_count: policy_scope(AgentReward).count,
+        items_on_page: PAGESIZE,
+        rewards: agent_rewards.page(rewards_params[:page])
+          .per(PAGESIZE)
+                              .as_json(include: { user: { include: { promo_code: { only: :code } },
+                                                          only: %i[username email] } },
+                                       only: %i[amount status_cd]),
+        rewards_total: agent_rewards.sum(&:amount),
+        rewards_payed: agent_rewards.select(&:payed?).sum(&:amount)
+      }
+    end
+
+    private
+
+    def rewards_params
+      params.permit(:page)
+    end
+  end
+end
