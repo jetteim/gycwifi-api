83348d3104cc4d416afdeb6c9144bbbf312357e7
Maxim Lee <mlee@key-g.com>
2017-08-29 05:46:12 +0300

    add twitter logging

--- lib/oauth/twitter.rb
+++ lib/oauth/twitter.rb
@@ -1,8 +1,8 @@
 module Oauth
   class TwitterLibrary
     def self.request_token(params)
-      twitter_key = 'wrJz64NqtUnezFE2m86toABdV'
-      twitter_secret = '4bDGx7GXDi1ouCpudpVx2qmeaMCX4ZzkHuos9fvQdFk3b1Zw0Y'
+      twitter_key = 'fI1qz0H8qdBhk95AFVE1b1S3k'
+      twitter_secret = 'ZLeoiweNZgw26pXXstC4qGbjAUD5NEb2aYYfDMWcwHQ5ILMHMN'
       result = JSON.parse(RestClient::Request.execute(
                             method: :post,
                             url: 'https://api.twitter.com/oauth/request_token',
@@ -12,24 +12,19 @@ module Oauth
       ))
     end
 
-    def self.user_data(_access_code, _redirect_url)
+    def self.user_data(access_code, redirect_url)
       # bearer_token_credentials = "#{ENV['TWITTER_KEY']}:#{ENV['TWITTER_SECRET']}"
-      twitter_key = 'wrJz64NqtUnezFE2m86toABdV'
-      twitter_secret = '4bDGx7GXDi1ouCpudpVx2qmeaMCX4ZzkHuos9fvQdFk3b1Zw0Y'
-      access_token = JSON.parse(RestClient::Request.execute(
-                                  method: :post,
-                                  url: 'https://api.twitter.com/oauth/access_token',
-                                  oauth_token: params['oauth_token'],
-                                  oauth_token_secret: params['oauth_token_secret'],
-                                  user: twitter_key,
-                                  password: twitter_secret
-      ))
-      auth_result = RestClient::Request.execute(
-        method: :get,
-        url: 'https://api.twitter.com/1.1/account/verify_credentials.json?include_email=true',
-        headers: { 'Authorization': "Bearer #{result['access_token']}" }
+      Rails.logger.debug "twitter auth, access code #{access_code}, redirect_url #{redirect_url}".green
+      # twitter_key = 'wrJz64NqtUnezFE2m86toABdV'
+      # twitter_secret = '4bDGx7GXDi1ouCpudpVx2qmeaMCX4ZzkHuos9fvQdFk3b1Zw0Y'
+      token_request = JSON.parse(
+        RestClient.post('https://api.twitter.com/oauth/access_token',
+                        client_id: ENV['TWITTER_KEY'],
+                        client_secret: ENV['TWITTER_SECRET'],
+                        code: access_code,
+                        redirect_uri: redirect_url), symbolize_names: true
       )
-      JSON.parse(auth_result).deep_symbolize_keys
+      Rails.logger.debug "got reply: #{token_request}".green
     end
   end
 end
