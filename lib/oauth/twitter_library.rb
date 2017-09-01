9e9b14a245c80e825df2ada4408859c4e12a1d7e
Maxim Lee <mlee@key-g.com>
2017-09-01 21:18:37 +0300

    twitter delayed job support

--- lib/oauth/twitter_library.rb
+++ lib/oauth/twitter_library.rb
@@ -24,15 +24,15 @@ module Oauth
     def self.user_data(oauth_token:, oauth_verifier:)
       Rails.logger.debug "request_token: #{@request_token.inspect}".red
 
-      Rails.logger.debug "REDIS stored token: #{token = REDIS.get(redis_token_key(@request_token&.token || oauth_token))}".green
+      Rails.logger.debug "REDIS stored secret: #{secret = REDIS.get(redis_token_key(@request_token&.token || oauth_token))}".green
 
       Rails.logger.debug "instance contains user data: #{@user_data.inspect}".green if @user_data
       return @user_data if @user_data
 
-      Rails.logger.debug "REDIS stored data: #{redis_data.inspect}".green if redis_data = REDIS.get(redis_user_data_key(@request_token&.token || token))
+      Rails.logger.debug "REDIS stored data: #{redis_data.inspect}".green if redis_data = REDIS.get(redis_user_data_key(@request_token&.token || oauth_token))
       return JSON.parse(redis_data, symbolize_keys: true) if redis_data
 
-      options = { oauth_token: @request_token.token, oauth_token_secret: @request_token.secret, oauth_verifier: oauth_verifier }
+      options = { oauth_token: @request_token.token || oauth_token, oauth_token_secret: @request_token.secret || secret,  oauth_verifier: oauth_verifier }
       @request_token = OAuth::RequestToken.from_hash(@@consumer, options)
       access_token = @request_token.get_access_token(options)
       Rails.logger.debug "access token: #{access_token.inspect}".magenta
